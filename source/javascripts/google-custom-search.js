b93cafd1891d9e6a1f0936f754325f832889172a
David Laing <david@davidlaing.com>
2013-03-10 00:49:32 +0000

    Add Google custom search box

---
+++ source/javascripts/google-custom-search.js
@@ -0,0 +1,12 @@
+/* Google Custom Search
+* Custom search set up under mrdavidlaing@gmail.com - email for details of sites indexed */
+(function() {
+  var cx = '011496095511695732182:yllu-lq9bfq'; 
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
+      '//www.google.com/cse/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
